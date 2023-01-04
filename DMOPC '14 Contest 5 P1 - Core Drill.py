ee96d47a7bd8cc323e6f7c0af98b1dfdbabd35d3
seondays <agseon9@gmail.com>
2023-01-04 17:13:13 +0900

    feat : DMOJ Core Drill

---
+++ DMOPC '14 Contest 5 P1 - Core Drill.py
@@ -0,0 +1,10 @@
+# https://dmoj.ca/problem/dmopc14c5p1
+import math
+
+radius = int(input())
+height = int(input())
+pi = math.pi
+
+volume = (pi*(radius**2)*height)/3
+
+print(volume)
--- DMOPC '15 Contest 7 P2 - Not a Wall of Text.py
+++ DMOPC '15 Contest 7 P2 - Not a Wall of Text.py
@@ -1,3 +1,5 @@
+# https://dmoj.ca/problem/dmopc15c7p2
+
 input = input()
 
 split_count_result = len(input.split(" "))
