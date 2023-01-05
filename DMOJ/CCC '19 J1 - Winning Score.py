7debcb1ce3277ebeb04bb6e4796a21f6531a399e
seondays <agseon9@gmail.com>
2023-01-05 20:16:45 +0900

    style :  코딩 컨벤션 적용하여 수정

--- DMOJ/CCC '19 J1 - Winning Score.py
+++ DMOJ/CCC '19 J1 - Winning Score.py
@@ -8,7 +8,7 @@ second_team_score_2 = int(input())
 second_team_score_1 = int(input())
 
 def calculate_score(first,second,thrid):
-    score = first * 3 + second * 2 + thrid
+    score = first*3 + second*2 + thrid
     return score
 
 if calculate_score(first_team_score_3,first_team_score_2,first_team_score_1) > calculate_score(second_team_score_3,second_team_score_2,second_team_score_1):
