8f1349263055044e76bbd398aed7237c6bdacfb1
seondays <agseon9@gmail.com>
2023-03-16 23:57:31 +0900

    incorrect : DOMJ Cezar

---
+++ DMOJ/COCI '17 Contest 1 #1 Cezar.py
@@ -0,0 +1,33 @@
+#https://dmoj.ca/problem/coci17c1p1
+
+cards_count = {1:4,2:4,3:4,4:4,5:4,6:4,7:4,8:4,9:4,10:12,11:4}
+
+drawn_count = int(input())
+sum_power = 0
+
+for i in range(drawn_count):
+    drawn = int(input())
+
+    sum_power += drawn
+    cards_count[drawn] = cards_count[drawn]-1
+
+sum_power = 21 - sum_power
+lower_power = 0
+
+if sum_power < 11:
+    for i in range(1,sum_power+1):
+        lower_power += cards_count[i]
+        
+    higher_power = sum(cards_count.values()) - lower_power
+
+    if lower_power < higher_power :
+        print("DOSTA")
+    else:
+        print("VUCI")
+
+else:
+    print("VUCI")
+
+
+
+
