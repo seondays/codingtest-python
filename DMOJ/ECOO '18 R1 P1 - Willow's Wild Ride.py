1759ac1bdc06d23104945a759ca8f6690c00681a
seondays <agseon9@gmail.com>
2023-03-20 21:27:52 +0900

    feat : DMOJ willow's wild ride

---
+++ DMOJ/ECOO '18 R1 P1 - Willow's Wild Ride.py
@@ -0,0 +1,23 @@
+# https://dmoj.ca/problem/ecoo18r1p1
+
+for j in range(10) :
+    input_data = input().split(" ")
+
+    playing_days = int(input_data[0])
+    next_days = int(input_data[1])
+
+    habits = []
+    willow_playing = 0
+
+    for i in range(next_days):
+        today = input()
+        habits.append(today)
+
+    for i in habits:
+        if i == "B":
+            willow_playing += playing_days
+            
+        if willow_playing > 0:
+            willow_playing -= 1
+
+    print(willow_playing)
