62cd5482548284c3e0ea737c471e383e534ad805
seondays <agseon9@gmail.com>
2023-04-10 23:18:34 +0900

    feat : Timus 2144 상자들이 차례대로 정리가 가능한지 확인

--- DMOJ/Timus 2144. Cleaning the Room.py
+++ DMOJ/Timus 2144. Cleaning the Room.py
@@ -8,14 +8,36 @@ def read_box(n):
         boxes.append(box)
     return boxes
 
-count = int(input())
-boxes = read_box(count)
-
-# TODO : 상자 내부가 차례대로 정리되어 있는지 확인
 def is_box_sorted(boxes):
     for i in range(len(boxes)):
         if boxes[i] != sorted(boxes[i]):
             return False
     return True
 
-# TODO : 상자들이 차례대로 정리가 가능한지 확인        
+def make_list_start_end_in_box(boxes):
+    new_list = []
+    for i in range(len(boxes)):
+        list = []
+        list.append(boxes[i][0])
+        list.append(boxes[i][-1])
+        new_list.append(list)
+    return sorted(new_list)
+
+def is_boxes_sorted(boxes):
+    for i in range(len(boxes)-1):
+        if boxes[i][-1] > boxes[i+1][0]:
+            return False
+    return True
+
+# Main Program
+
+count = int(input())
+boxes = read_box(count)
+
+if is_box_sorted(boxes):
+    if is_boxes_sorted(make_list_start_end_in_box(boxes)):
+        print("Yes")
+    else:
+        print("No")
+else:
+    print("No")
