35cc48dfd26bd8a47a941402f3678b4d63579de0
seondays <agseon9@gmail.com>
2023-04-12 22:41:38 +0900

    feat : From 1987 to 2013 : 고유의 숫자인지 확인한 후 출력

--- DMOJ/CCC '13 S1 - From 1987 to 2013.py
+++ DMOJ/CCC '13 S1 - From 1987 to 2013.py
@@ -9,3 +9,22 @@ def change_str_to_int(input):
 year = change_str_to_int(input())
 
 # TODO : 고유의 숫자인지 확인하기
+
+def is_distinct_digits(list):
+    '''리스트가 고유한 숫자만으로 이루어져 있는지 여부를 확인합니다.'''
+    
+    for i in list:
+        if list.count(i) > 1:
+            return False
+    
+    return True
+
+is_distinct = False
+
+while is_distinct is False:
+    if is_distinct_digits(list(year)):
+        is_distinct = True
+    else:
+        year = change_str_to_int(year)
+
+print(year)
