387eecc661e89fbf0a2566c0d1ad228b084bc685
seondays <agseon9@gmail.com>
2023-04-29 23:37:23 +0900

    feat : DMOJ Crtanje 2nd

--- review/COCI '20 Contest 2 #1 Crtanje.py
+++ review/COCI '20 Contest 2 #1 Crtanje.py
@@ -1,42 +1,33 @@
 # https://dmoj.ca/problem/coci20c2p1
 
-input_len = int(input())
-input_sentence = input()
+#입력 처리
+days = int(input())
+characters = input()
 
-pictures = []
-coordinate = []
-standard = 0
+#기본 그래프 틀 그리기
+matrix = []
 
-for i in input_sentence:
-    if i == '+':
-        coordinate.append(standard)
-        standard += 1
-    if i == '-':
-        standard -= 1
-        coordinate.append(standard)
-    if i == '=':
-        coordinate.append(standard)
+for i in range(2*(days + 1)):
+    dot_list = list('.'*days)
+    matrix.append(dot_list)
 
-min_value = min(coordinate)
+#그래프 내용 작성하기
+col = days + 1
 
-coordinate = list(map(lambda x:x-min_value,coordinate))
-row_count = max(coordinate)+1
+for i in range(len(characters)):
+    if characters[i] == '+':
+        matrix[col][i] = '/'
+        col += 1
+    if characters[i] == '-':
+        col -= 1
+        matrix[col][i] = '\\'
+    if characters[i] == '=':
+        matrix[col][i] = '_'
 
-for i in range(row_count):
-    row = ['.' for j in range(input_len)]
-    pictures.append(row)
-
-for i in range(input_len):
-    if input_sentence[i] == '+':
-        pictures[coordinate[i]][i] = '/'
-    if input_sentence[i] == '-':
-        pictures[coordinate[i]][i] = '\\'
-    if input_sentence[i] == '=':
-        pictures[coordinate[i]][i] = '_'
-
-for i in range(len(pictures)-1,-1,-1):
-    print(''.join(pictures[i]))
-        
+# main
+for i in range(len(matrix)-1,-1,-1):
+    if matrix[i].count('.') != days:
+        print(''.join(matrix[i]))
     
     
 
