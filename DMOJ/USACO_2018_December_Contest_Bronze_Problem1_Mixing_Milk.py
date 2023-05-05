8f5029b3a480f65214613bdb49dcb56960023653
seondays <agseon9@gmail.com>
2023-05-05 22:57:27 +0900

    refactor : USACO 2018 dec B1 : 함수 리펙터링

--- DMOJ/USACO_2018_December_Contest_Bronze_Problem1_Mixing_Milk.py
+++ DMOJ/USACO_2018_December_Contest_Bronze_Problem1_Mixing_Milk.py
@@ -9,19 +9,15 @@ def read_bucket(input_file):
 
 # 버켓 섞기
 def mix_bucket(first_bucket,secound_bucket):
-    first_bucket_capacity = first_bucket[0]
     first_bucket_amount = first_bucket[1]
     secound_bucket_capacity = secound_bucket[0]
     secound_bucket_amount = secound_bucket[1]
     
-    secound_bucket_amount += first_bucket_amount
-    first_bucket_amount = 0
+    milk = min(first_bucket_amount,secound_bucket_capacity-secound_bucket_amount)
+    first_bucket_amount -= milk
+    secound_bucket_amount += milk
+    after_mixed = [first_bucket_amount,secound_bucket_amount]
     
-    if secound_bucket_amount > secound_bucket_capacity:
-        overflow = secound_bucket_amount - secound_bucket_capacity
-        first_bucket_amount += overflow
-        secound_bucket_amount = secound_bucket_capacity
-    after_mixed = [[first_bucket_capacity,first_bucket_amount],[secound_bucket_capacity,secound_bucket_amount]]
     return after_mixed
         
 # Main
@@ -32,15 +28,11 @@ milk_buckets = read_bucket(input_file)
 
 for i in range(100):
     first_number = i % 3
-    
-    if first_number == 2:
-        secound_number = 0
-    else :
-        secound_number = first_number + 1
+    secound_number = (i+1) % 3
     
     after_mixed = mix_bucket(milk_buckets[first_number],milk_buckets[secound_number])
-    milk_buckets[first_number] = after_mixed[0]
-    milk_buckets[secound_number] = after_mixed[1]
+    milk_buckets[first_number][1] = after_mixed[0]
+    milk_buckets[secound_number][1] = after_mixed[1]
 
 for i in range(3):
     output_file.write(str(milk_buckets[i][1])+'\n')
