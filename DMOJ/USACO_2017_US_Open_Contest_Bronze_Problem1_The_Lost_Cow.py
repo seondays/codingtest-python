8f6fdccb6979497de2a4f020497497b5c22fda65
seondays <agseon9@gmail.com>
2023-05-09 15:51:44 +0900

    fix : USACO 2017 Open : x>y 일 경우 처리기능 추가

--- DMOJ/USACO_2017_US_Open_Contest_Bronze_Problem1_The_Lost_Cow.py
+++ DMOJ/USACO_2017_US_Open_Contest_Bronze_Problem1_The_Lost_Cow.py
@@ -10,18 +10,13 @@ def calculate_location(x,count,zigzag):
     return x
 
 # 이동거리 구하기
-def moving(location,zigzag_location,y):
-    move = 0
-    if zigzag_location > y:
-        move = abs(y - location)
-        return move
-    if zigzag_location <= y:
-        move = abs(zigzag_location-location)
-        return move
+def moving(location,zigzag_location):
+    move = abs(location - zigzag_location)
+    return move
     
 # main
-input_file = open('DMOJ/lostcow.in','r')
-output_file = open('DMOJ/lostcow.out','w')
+input_file = open('lostcow.in','r')
+output_file = open('lostcow.out','w')
 
 location_info = list(map(int,input_file.readline().split()))
 x = location_info[0]
@@ -31,11 +26,23 @@ zigzag = 1
 count = 1
 total_move = 0
 
-while location < y:
-    zigzag_location = calculate_location(x,count,zigzag)
-    total_move += moving(location,zigzag_location,y)
-    location = zigzag_location
-    count += 1
-    zigzag *= 2
+if x < y :
+    while location < y:
+        zigzag_location = calculate_location(x,count,zigzag)
+        total_move += moving(location,zigzag_location)
+        if zigzag_location > y:
+            total_move -= (zigzag_location-y)
+        location = zigzag_location
+        count += 1
+        zigzag *= 2
+elif x > y :
+    while location > y:
+        zigzag_location = calculate_location(x,count,zigzag)
+        total_move += moving(location,zigzag_location)
+        if zigzag_location < y:
+            total_move -= (y-zigzag_location)
+        location = zigzag_location
+        count += 1
+        zigzag *= 2
 
 output_file.write(str(total_move))
