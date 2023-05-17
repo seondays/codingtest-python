110aac30c7e271161b109a21cfeb6136ebfc7919
seondays <agseon9@gmail.com>
2023-05-17 19:49:44 +0900

    feat : USACO 2019 Jan B1 : 라운드 진행 함수들 구현

--- DMOJ/USACO_2019_January_Contest_Bronze_Problem1_Shell_Game.py
+++ DMOJ/USACO_2019_January_Contest_Bronze_Problem1_Shell_Game.py
@@ -5,3 +5,18 @@ output_file = open('shell.out','w')
 
 n = int(input_file.readline().strip())
 round = [[int(j) for j in i.split()] for i in input_file.read().strip().split('\n')]
+
+def set_round(n):
+    setting = [0,0,0]
+    setting[n-1] = 1
+    return setting
+
+def play_round(swap,answer_now):
+    result = answer_now
+    result[swap[1]-1],result[swap[0]-1] = result[swap[0]-1],result[swap[1]-1]
+    return result
+
+def check_answer(answer,predict):
+    if answer[predict-1] :
+        return True
+    return False
