846b116da4328c12b96d490ebbab57a7e26b8fa7
seondays <agseon9@gmail.com>
2023-05-23 22:38:08 +0900

    feat : DMOJ Are we There yet (2nd)

--- review/CCC '18 J3 - Are we there yet.py
+++ review/CCC '18 J3 - Are we there yet.py
@@ -1,23 +1,16 @@
 # https://dmoj.ca/problem/ccc18j3
 
-# TODO : 입력받아서, 리스트로 저장
-distance = list(map(int,input().split(" ")))
+distance = list(map(int,input().split()))
 
-# TODO : 마을 주소를 구한다.
+towns = [0]
 
-cities = [0]
+for i in range(len(distance)):
+    town = distance[i] + towns[i]
+    towns.append(town)
 
-for i in range(4):
-    cities.append(cities[i]+distance[i])
-
-# TODO : i번째 마을과 얼마나 떨어져있는지 계산하여 출력하기
-
-for i in range(5):
-    a = []
-    for j in range(5):
-        d = cities[i] - cities[j]
-        if d < 0:
-            d *=  -1
-        a.append(d)
-    
-    print(*a)
+for i in range(len(towns)):
+    d = []
+    for j in range(len(towns)):
+        between = towns[i] - towns[j]
+        d.append(between)
+    print(*list(map(lambda x : abs(x),d)))
