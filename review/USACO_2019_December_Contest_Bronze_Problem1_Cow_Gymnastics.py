1421ac1c96797826c429b16d773181512f7dd1e7
seondays <agseon9@gmail.com>
2023-08-06 23:58:13 +0900

    feat : USACO 2019 Dec P1 빈도수 저장

--- review/USACO_2019_December_Contest_Bronze_Problem1_Cow_Gymnastics.py
+++ review/USACO_2019_December_Contest_Bronze_Problem1_Cow_Gymnastics.py
@@ -1,26 +1,31 @@
 # http://usaco.org/index.php?page=viewproblem2&cpid=963
 
-input_file = open('gymnastics.in','r')
-output_file = open('gymnastics.out','w')
+input_file = open('DMOJ/gymnastics.in','r')
+output_file = open('DMOJ/gymnastics.out','w')
 
-number_of_sessions, number_of_cows = [int(i) for i in input_file.readline().split()]
-sessions = []
-for i in range(number_of_sessions):
-        sessions.append(list(int(i)-1for i in input_file.readline().split()))
+n = input_file.readline().split()
+sessions = int(n[0])
+performance = int(n[1])
+dict = {}
 
-check = [[0 for k in range(number_of_cows)] for i in range(number_of_cows)]
-for i in range(number_of_sessions):
-    for j in range(number_of_cows):# cow1
-            for k in range(j+1,number_of_cows):# cow2
-                   index1=sessions[i][j]
-                   index2=sessions[i][k]
-                   check[sessions[i][j]][sessions[i][k]] += 1
+for i in range(sessions):
+    session = list(map(int,input_file.readline().split()))
+    for j in range(performance):
+        for k in range(j+1,performance):
+            
+                tmp = []
+                tmp.append(session[j])
+                tmp.append(session[k])
+                tmp = tuple(tmp)
 
-count = 0
-for i in check:
-       count += i.count(number_of_sessions)
+                if tmp in dict:
+                      dict[tmp] += 1
+                else:
+                      dict[tmp] = 1
 
-output_file.write(str(count))
+print(dict)
+
+
 
 input_file.close()
 output_file.close()
