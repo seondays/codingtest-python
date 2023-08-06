20167b138cb5135553339bfe5ef4e6e6df4bc911
seondays <agseon9@gmail.com>
2023-08-07 00:24:14 +0900

    refactor : USACO 2019 Dec P1 튜플 변환 방식 수정

--- DMOJ/USACO_2019_December_Contest_Bronze_Problem1_Cow_Gymnastics.py
+++ DMOJ/USACO_2019_December_Contest_Bronze_Problem1_Cow_Gymnastics.py
@@ -1,31 +1,23 @@
 # http://usaco.org/index.php?page=viewproblem2&cpid=963
 
-input_file = open('DMOJ/gymnastics.in','r')
-output_file = open('DMOJ/gymnastics.out','w')
+input_file = open('gymnastics.in','r')
+output_file = open('gymnastics.out','w')
 
 n = input_file.readline().split()
 sessions = int(n[0])
 performance = int(n[1])
 dict = {}
+result = 0
 
 for i in range(sessions):
     session = list(map(int,input_file.readline().split()))
     for j in range(performance):
         for k in range(j+1,performance):
-            
-                tmp = []
-                tmp.append(session[j])
-                tmp.append(session[k])
-                tmp = tuple(tmp)
-
+                tmp = (session[j],session[k])
                 if tmp in dict:
                       dict[tmp] += 1
                 else:
                       dict[tmp] = 1
-
-print(dict)
-
-
 
 input_file.close()
 output_file.close()
