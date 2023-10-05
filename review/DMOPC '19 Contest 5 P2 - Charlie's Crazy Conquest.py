c30ec3d77b52dc9ff39b79727d0164f3c7481404
seondays <agseon9@gmail.com>
2023-10-05 23:42:41 +0900

    feat : 입력 전처리

--- review/DMOPC '19 Contest 5 P2 - Charlie's Crazy Conquest.py
+++ review/DMOPC '19 Contest 5 P2 - Charlie's Crazy Conquest.py
@@ -1,68 +1,29 @@
 # https://dmoj.ca/problem/dmopc19c5p2
 
-input_text = input().split(" ")
+info = list(map(int,input().split()))
+turn = info[0]
 
-turn = int(input_text[0])
-player_health = int(input_text[1])
-enemy_health = int(input_text[1])
+# 플레이어와 적의 체력 설정
+player_hp = info[1]
+enemy_hp = info[0]
 
-player_move = []
-player_damage = []
-enemy_move = []
-enemy_damage = []
-
-for i in range(1,turn*2+1):
-    move = input().split(" ")
-    if i <= turn:
-        player_move.append(move[0])
-        player_damage.append(int(move[1]))
-    
-    if i > turn:
-        enemy_move.append(move[0])
-        enemy_damage.append(int(move[1]))
-
-move = []
-damage = []
+# 플레이어의 행동 리스트 설정
+player_act = []
+player_val = []
 
 for i in range(turn):
-    move.append(player_move[i])
-    move.append(enemy_move[i])
-    damage.append(player_damage[i])
-    damage.append(enemy_damage[i])
+    info = input().split()
+    player_act.append(info[0])
+    player_val.append(int(info[1]))
 
+# 적의 행동 리스트 설정
+enemy_act = []
+enemy_val = []
 
-for i in range(len(move)):
-    
-    if i % 2 == 0:
-        if i == 0 and move[i] == 'A':
-            enemy_health -= damage[i]
-        if i != 0 and move[i] == 'A':
-            if move[i-1] == 'A':
-                enemy_health -= damage[i]
-        if move[i] == 'D':
-            if move[i+1] == 'D':
-                player_health -= damage[i]
-    
-    if i % 2 != 0:
-        if move[i] == 'A':
-            if move[i-1] == 'A':
-                player_health -= damage[i]
-        if move[i] == 'D' and i == len(move)-1:
-            enemy_health -= damage[i]
-        if move[i] == 'D' and i != len(move)-1:
-            if move[i+1] == 'D':
-                enemy_health -= damage[i]
-       
-    if player_health <= 0 :
-        print("DEFEAT")
-        break
-    if enemy_health <= 0 :
-        print("VICTORY")
-        break
-
-if player_health > 0 and enemy_health > 0 :
-    print("TIE")   
-        
+for i in range(turn):
+    info = input().split()
+    enemy_act.append(info[0])
+    enemy_val.append(int(info[1]))
     
     
     
