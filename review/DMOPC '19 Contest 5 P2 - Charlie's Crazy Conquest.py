43d9a0feb4bde1d8aa9320f60514fcd111abbcf1
seondays <agseon9@gmail.com>
2023-10-06 00:31:12 +0900

    feat : 최종 행동 리스트 생성

--- review/DMOPC '19 Contest 5 P2 - Charlie's Crazy Conquest.py
+++ review/DMOPC '19 Contest 5 P2 - Charlie's Crazy Conquest.py
@@ -5,7 +5,7 @@ turn = info[0]
 
 # 플레이어와 적의 체력 설정
 player_hp = info[1]
-enemy_hp = info[0]
+enemy_hp = info[1]
 
 # 플레이어의 행동 리스트 설정
 player_act = []
@@ -24,9 +24,14 @@ for i in range(turn):
     info = input().split()
     enemy_act.append(info[0])
     enemy_val.append(int(info[1]))
-    
-    
-    
-    
-    
 
+# 최종 행동 리스트 생성
+act = ['dummy']
+val = [-1]
+for i in range(turn):
+    act.append(player_act[i])
+    act.append(enemy_act[i])
+    val.append(player_val[i])
+    val.append(enemy_val[i])
+act.append('dummy')
+val.append(-1)
